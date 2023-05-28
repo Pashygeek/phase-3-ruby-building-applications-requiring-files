f232ff5bd03a4d510b5f3faaca931fe41a31694e
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-28 15:35:07 +0300

    Completed assignment

--- lib/garden.rb
+++ lib/garden.rb
@@ -14,6 +14,21 @@ class Garden
   end
 end
 
+class Plant
+  attr_accessor :garden, :name
+
+  @@all = []
+
+  def initialize(name:)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
 lawn = Garden.new(name: 'Front Lawn')
 
 basil = Plant.new(name: 'Basil')
